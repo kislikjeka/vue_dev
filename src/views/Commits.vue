348328d6a55d91cdb6a5d1bd304089eee01af5d3
kislikjeka <evgenijkislicenko@Evgenijs-MacBook-Pro.local>
2019-07-18 23:59:04 +0300

    Change com view to data-table with search

--- src/views/Commits.vue
+++ src/views/Commits.vue
@@ -11,38 +11,30 @@
           <v-icon>refresh</v-icon>
         </v-btn>
       </v-layout>
-      <v-expansion-panel popout>
-        <v-expansion-panel-content v-for="com in commits" :key="com.sha">
-          <template v-slot:header>
-            <div>{{com.commit.message}}</div>
+      <v-card>
+        <v-card-title class="grey lighten-4">
+          <v-text-field
+            v-model="search"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :search="search"
+          :headers="headers"
+          :items="commits"
+          :pagination.sync="pagination"
+          class="elevation-1"
+        >
+          <template v-slot:items="props">
+            <td>{{ props.item.commit.author.name}}</td>
+            <td class="text-xs-left">{{ props.item.commit.message }}</td>
+            <td class="text-xs-left">{{ new Date(props.item.commit.author.date).toLocaleString()}}</td>
           </template>
-          <v-divider></v-divider>
-          <v-card>
-            <v-layout row wrap>
-              <v-flex xs4>
-                <v-subheader>Author:</v-subheader>
-                <v-card-text>
-                  <v-avatar size="36" v-if="com.author !=null">
-                    <img v-bind:src="com.author.avatar_url" />
-                  </v-avatar>
-                  <v-avatar size="36" v-else>
-                    <v-icon color="indigo">person</v-icon>
-                  </v-avatar>
-                  <span class="pl-2">{{com.commit.author.name}}</span>
-                </v-card-text>
-              </v-flex>
-              <v-flex xs4>
-                <v-subheader>Message:</v-subheader>
-                <v-card-text>{{com.commit.message}}</v-card-text>
-              </v-flex>
-              <v-flex xs4>
-                <v-subheader>Date/Time:</v-subheader>
-                <v-card-text>{{com.commit.author.date.split("T")[0] + "/" +com.commit.author.date.split("T")[1].split("Z")[0]}}</v-card-text>
-              </v-flex>
-            </v-layout>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
+        </v-data-table>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -51,7 +43,23 @@
 export default {
   data() {
     return {
-      commits: []
+      pagination: {
+        rowsPerPage: -1,
+        sortBy: "commit.author.date",
+        descending: true
+      },
+      search: "",
+      commits: [],
+      headers: [
+        {
+          text: "Login",
+          align: "left",
+          sortable: false,
+          value: "name"
+        },
+        { text: "Message", value: "commit.message" },
+        { text: "Date/Time", value: "commit.author.date" }
+      ]
     };
   },
   methods: {
